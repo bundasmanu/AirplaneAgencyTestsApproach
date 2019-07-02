c8aec7b8f34d15ed31b4efa9fe4ce3f7a31e8d17
gustavo <gustavo@debiancli>
2019-07-02 18:16:05 +0100

    Casos de Teste validados

--- AgencyClient/test/BvaEctCase2.java
+++ AgencyClient/test/BvaEctCase2.java
@@ -102,17 +102,17 @@ public class BvaEctCase2 {
             {"Madrid", "Porto", 50, 0 , FeedbackResult.ValidFeedback},
             {"Madrid", "Madrid", 50, 0 , FeedbackResult.ValidFeedback},
             {"Porto", "Porto", 200 , 5 , FeedbackResult.InvalidFeedback}, /*FALHA 19--> O SISTEMA DEIXA COMPRAR BILHETE, MSM DPS DE ADQUIRIR DA VIAGEM TER TERMINADO E NAO VALIDADO MESMO DESTINO E PARTIDA*/
-            {"Porto", "Madrid", 200, 6 , FeedbackResult.ValidFeedback},
+            {"Porto", "Madrid", 200, 6 , FeedbackResult.ValidFeedback}, 
             {"Madrid", "Porto", 200, 7 , FeedbackResult.ValidFeedback},
             {"Madrid", "Madrid", 200, 9 , FeedbackResult.InvalidFeedback}, /*FALHA 22--> DEIXA COMPRAR BILHETES COM O MSM DESTINO E PARTIDA*/
             {"Porto", "Porto", 50 , 5 , FeedbackResult.InvalidFeedback},
             {"Porto", "Madrid", 50, 6 , FeedbackResult.ValidFeedback},
             {"Madrid", "Porto", 50, 8 , FeedbackResult.ValidFeedback},
             {"Madrid", "Madrid", 50, 9 , FeedbackResult.InvalidFeedback}, /*FALHA 26--> DEIXA COMPRAR BILHETES COM O MSM DESTINO E PARTIDA*/
-            /*WEAK ROBUST*/
+//            /*WEAK ROBUST*/
             {"Moscovo", "Porto", 200 , 10 , FeedbackResult.InvalidFeedback}, /*NAO FALHA, MAS DEVIA FALHAR 27--> TESTE IMPOSSIVEL DE TESTAR CÓDIGO--> NAO É POSSÍVEL TESTAR A COMPRA DE UMA VIAGEM SEM 2 CIDADES DE DESTINO*/
             {"Porto", "Moscovo", 50 , 0 , FeedbackResult.InvalidFeedback}, /*FALHA 28--> TESTE IMPOSSIVEL DE TESTAR CÓDIGO--> NAO É POSSÍVEL TESTAR A COMPRA DE UMA VIAGEM SEM 2 CIDADES DE DESTINO*/
-            {"Madrid", "Porto", 50, -10 , FeedbackResult.InvalidFeedback},
+//            {"Madrid", "Porto", 50, -10 , FeedbackResult.InvalidFeedback},/*TESTE ESTUPIDO DE SER REALIZADO--> COMPRAR BILHETES NEGATIVOS??*/
             {"Porto", "Madrid", 200, 11 , FeedbackResult.InvalidFeedback}
         });
     }
@@ -164,6 +164,7 @@ public class BvaEctCase2 {
             
         }
         catch(Exception e){
+            assertFalse("", true);
             System.out.println(e.getMessage());
         }
         
